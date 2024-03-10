4b72bed94905eb4252ec5d18dc85a08257df4e08
seungwoneeee <130162597+seungwoneeee@users.noreply.github.com>
2024-03-10 22:48:20 +0900

    [level 0] Title: 소인수분해, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120852. 소인수분해/소인수분해.py
@@ -0,0 +1,23 @@
+
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+def solution(n):
+    answer = []
+    d = 2
+    while d <= n:
+        if n % d == 0:
+            n /= d
+            if d not in answer:
+                answer.append(d)
+        else:
+            d += 1
+    return answer
