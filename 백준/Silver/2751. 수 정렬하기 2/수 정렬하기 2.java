de8d5e80c7945a8e0bd00e79352a3a7f9758a79e
seungwoneeee <130162597+seungwoneeee@users.noreply.github.com>
2025-03-12 15:16:55 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 1016 ms, Memory: 119092 KB -BaekjoonHub

--- 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.java
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.java
@@ -2,59 +2,59 @@ import java.util.*;
 import java.io.*;
 
 public class Main{
-    public static int[] A, tmp;
-    public static void main(String[] args) throws IOException{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        //병합정렬로 풀어보기
-        int N = Integer.parseInt(br.readLine());
-        A = new int[N+1];
-        tmp = new int[N+1]; //정렬 시 사용할 임시 배열
-        for(int i=1;i<=N;i++){
-            A[i]=Integer.parseInt(br.readLine());
-        }
-        mergetSort(1,N); //sort할 시작부터 끝까지 매개변수로 전달
-        for(int i=1;i<=N;i++){
-            bw.write(A[i]+"\n");
-        }
-        bw.flush();
-        bw.close();
-    }
-    public static void mergetSort(int s, int e){
-        if(e-s<1) //재귀함수를 종료하는 조건, 만약 배열의 원소가 하나라면 재귀함수를 종료하도록 한다.
-            return;
-        int m = s+(e-s)/2; //집합을 여러개로 나누는 기준점이 된다.
-        //오버플로우 방지를 위해 (s+e)/2보다 s+(e-s)/2와 같은 식을 쓴다.
-        //재귀함수 형태로 구현
-        mergetSort(s,m);
-        mergetSort(m+1,e);
-        for(int i=s;i<=e;i++){
-            tmp[i]=A[i];
-        }
-        int k=s;
-        int index1=s;
-        int index2=m+1;
-        while(index1<=m&&index2<=e){ //두 그룹을 병합
-            //양쪽 그룹의 index가 가리키는 값을 비교, 더 작은 수를 선택해서 배열에 저장하고 선택된 데이터의 index값을 오른 쪽으로 한 칸 이동하기
-            if(tmp[index1]>tmp[index2]){
-                A[k]=tmp[index2];
-                k++;
-                index2++;
-            }else{
-                A[k]=tmp[index1];
-                k++;
-                index1++;
-            }
-        }
-        while(index1<=m){ //한쪽 그룹이 모두 선택된 경우 나머지 값 정리하기
-            A[k]=tmp[index1];
-            k++;
-            index1++;
-        }
-        while(index2<=e){
-            A[k]=tmp[index2];
-            k++;
-            index2++;
-        }
-    }
+	public static int[] A,tmp;
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		int N = Integer.parseInt(br.readLine());
+		A = new int[N+1];
+		tmp = new int[N+1];
+		for(int i=1;i<=N;i++) {
+			A[i]=Integer.parseInt(br.readLine());
+		}
+		mergetSort(1,N); //재귀함
+		for(int i=1;i<=N;i++) {
+			bw.write(A[i]+"\n");
+		}
+		bw.flush();
+		bw.close();
+	}
+	private static void mergetSort(int s, int e) {
+		if(e-s<1) //재귀 호출의 종료 조건 역
+			return; //e==s가 되면 해당 구간에 원소가 하나만 있으므로 이미 정렬된 상태->분할 필요 x
+		int m = s+(e-s)/2; //전체 길이의 절반에 해당하는 위치를 계산  
+		mergetSort(s,m); //배열의 왼쪽 부분을 정렬  
+		mergetSort(m+1,e); //배열의 오른쪽 부분을 정렬    
+		for(int i=s;i<=e;i++) { //현재 구간 복사 
+			tmp[i]=A[i];
+		}
+		int k=s; //두 부분 배열을 비교하면서 작은 값을 A[k]에 삽입하고자 함 
+		int index1=s; //왼쪽 배열의 시작 위치 
+		int index2=m+1; //오른쪽 배열의 시작 위치 
+		while(index1<=m&&index2<=e) {
+			// index1이 m을 초과하거나 index2가 e를 초과하면 종료
+			//두 개의 부분 배열 중 하나가 먼저 소진되면 종료
+			//즉, 반대쪽 배열의 원소가 남아 있을 가능성이 있음. 
+			if(tmp[index1]>tmp[index2]) { //왼쪽 배열과 오른쪽 배열의 원소를 비교했을 때 
+				A[k]=tmp[index2]; //더 작은 걸 A[k]에 삽입 
+				k++; //그리고 k에 정렬 값이 들어갔으므로 k++
+				index2++; //그리고 오른쪽의 가장 왼쪽의 값이 배열에 들어갔으므로 인덱스를 ++
+			}else { //반대의 경우 
+				A[k]=tmp[index1];
+				k++;
+				index1++;
+			}
+		}
+		while(index1<=m) { //첫번째 while문을 돌리고 나서, 오른쪽 배열은 정렬이 끝났지만, 왼쪽 배열의 정렬이 끝나지 않았을 경우 이 while문을 수행  
+			A[k]=tmp[index1];
+			k++;
+			index1++;
+		}
+		while(index2<=e) { //첫번째 while문을 돌리고 나서, 왼쪽  배열은 정렬이 끝났지만, 오른 쪽 배열의 정렬이 끝나지 않았을 경우 이 while문을 수행 
+			A[k]=tmp[index2];
+			k++;
+			index2++;
+		}
+	}
 }
